71aec20f582c00134faae8bbfc68401902a42f55
Umair Tariq <umairdev010@gmail.com>
2026-02-07 05:44:18 +0500

    Starting a new project on saturday

---
+++ src/main/java/org/umair/hotel_mangement_system_in_spring_boot/HotelMangementSystemInSpringBootApplication.java
@@ -0,0 +1,16 @@
+package org.umair.hotel_mangement_system_in_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMangementSystemInSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelMangementSystemInSpringBootApplication.class, args);
+
+        System.out.println("\nWELLCOME TO HOTEL MANAGEMENT SYSTEM.\n");
+
+    }
+
+}
---
+++ src/test/java/org/umair/hotel_mangement_system_in_spring_boot/HotelMangementSystemInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umair.hotel_mangement_system_in_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMangementSystemInSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
