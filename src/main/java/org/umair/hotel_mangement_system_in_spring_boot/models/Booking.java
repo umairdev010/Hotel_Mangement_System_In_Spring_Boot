862ec94e6159234b5d381fd05a79b94e1e11bd50
Umair Tariq <umairdev010@gmail.com>
2026-02-08 05:00:02 +0500

    updating project on sunday morning

--- src/main/java/org/umair/hotel_mangement_system_in_spring_boot/models/Booking.java
+++ src/main/java/org/umair/hotel_mangement_system_in_spring_boot/models/Booking.java
@@ -1,7 +1,85 @@
 package org.umair.hotel_mangement_system_in_spring_boot.models;
 
+import java.time.LocalDate;
+
 public class Booking {
 
+    private String room_id;
+    private String customer_id;
+    private LocalDate check_in_date;
+    private LocalDate check_out_date;
+    private float price;
+    private String status;
 
+    public Booking() {
+    }
 
+    public Booking(String room_id, String customer_id, LocalDate check_in_date, LocalDate check_out_date, float price, String status) {
+        this.room_id = room_id;
+        this.customer_id = customer_id;
+        this.check_in_date = check_in_date;
+        this.check_out_date = check_out_date;
+        this.price = price;
+        this.status = status;
+    }
+
+    public String getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(String room_id) {
+        this.room_id = room_id;
+    }
+
+    public LocalDate getCheck_in_date() {
+        return check_in_date;
+    }
+
+    public void setCheck_in_date(LocalDate check_in_date) {
+        this.check_in_date = check_in_date;
+    }
+
+    public String getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(String customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public LocalDate getCheck_out_date() {
+        return check_out_date;
+    }
+
+    public void setCheck_out_date(LocalDate check_out_date) {
+        this.check_out_date = check_out_date;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "room_id='" + room_id + '\'' +
+                ", customer_id='" + customer_id + '\'' +
+                ", check_in_date=" + check_in_date +
+                ", check_out_date=" + check_out_date +
+                ", price=" + price +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/umair/hotel_mangement_system_in_spring_boot/models/Customer.java
+++ src/main/java/org/umair/hotel_mangement_system_in_spring_boot/models/Customer.java
@@ -2,6 +2,38 @@ package org.umair.hotel_mangement_system_in_spring_boot.models;
 
 public class Customer {
 
+    private String name;
+    private String email;
 
+    public Customer() {
+    }
 
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
