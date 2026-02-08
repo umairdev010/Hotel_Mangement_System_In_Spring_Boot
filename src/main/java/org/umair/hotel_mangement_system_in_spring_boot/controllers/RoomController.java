21be87ee111c7ad78d257d1fcd32395b7bcca84e
Umair Tariq <umairdev010@gmail.com>
2026-02-08 16:51:36 +0500

    updating project on sunday evening

--- src/main/java/org/umair/hotel_mangement_system_in_spring_boot/controllers/RoomController.java
+++ src/main/java/org/umair/hotel_mangement_system_in_spring_boot/controllers/RoomController.java
@@ -2,10 +2,7 @@ package org.umair.hotel_mangement_system_in_spring_boot.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.umair.hotel_mangement_system_in_spring_boot.models.Room;
 import org.umair.hotel_mangement_system_in_spring_boot.services.RoomService;
 import org.umair.hotel_mangement_system_in_spring_boot.utility.Message;
@@ -17,18 +14,23 @@ public class RoomController {
 
     private RoomService roomService;
 
-    public RoomController(RoomService roomService){
+    public RoomController(RoomService roomService) {
         this.roomService = roomService;
     }
 
     @PostMapping
-    public ResponseEntity<Responses> createRoom(@RequestBody Room room){
+    public ResponseEntity<Responses> createRoom(@RequestBody Room room) {
 
         Responses responses = roomService.createRoom(room);
         return parseResponse(responses);
 
     }
 
+    @GetMapping("/getall")
+    public ResponseEntity<Responses> getAllRooms() {
+        Responses responses = roomService.getAllRooms();
+        return parseResponse(responses);
+    }
 
     public ResponseEntity<Responses> parseResponse(Responses responses) {
 
