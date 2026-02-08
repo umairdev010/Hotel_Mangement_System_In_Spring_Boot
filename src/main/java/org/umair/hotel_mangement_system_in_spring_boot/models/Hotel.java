d72aa62af3dda205e174a2a0dd6a87a412987c42
Umair Tariq <umairdev010@gmail.com>
2026-02-08 07:22:59 +0500

    updating project on sunday morning

---
+++ src/main/java/org/umair/hotel_mangement_system_in_spring_boot/models/Hotel.java
@@ -0,0 +1,60 @@
+package org.umair.hotel_mangement_system_in_spring_boot.models;
+
+public class Hotel {
+    private int id;
+    private String name;
+    private String location;
+    private String description;
+
+
+    public Hotel() {
+    }
+
+    public Hotel(String name, String location, String description) {
+        this.name = name;
+        this.location = location;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+}
