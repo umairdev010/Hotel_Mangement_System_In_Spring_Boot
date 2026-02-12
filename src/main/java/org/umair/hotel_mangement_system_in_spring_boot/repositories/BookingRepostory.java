a814c7b84172f4ae92a693c543f12fc044f26a59
Umair Tariq <umairdev010@gmail.com>
2026-02-12 15:45:19 +0500

    updating project on thursday

---
+++ src/main/java/org/umair/hotel_mangement_system_in_spring_boot/repositories/BookingRepostory.java
@@ -0,0 +1,11 @@
+package org.umair.hotel_mangement_system_in_spring_boot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.umair.hotel_mangement_system_in_spring_boot.models.Booking;
+
+@Repository
+public interface BookingRepostory extends JpaRepository<Booking,Integer> {
+
+    Booking findById(int id);
+}
---
+++ src/main/java/org/umair/hotel_mangement_system_in_spring_boot/services/BookingService.java
@@ -0,0 +1,35 @@
+package org.umair.hotel_mangement_system_in_spring_boot.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.umair.hotel_mangement_system_in_spring_boot.models.Booking;
+import org.umair.hotel_mangement_system_in_spring_boot.repositories.BookingRepostory;
+import org.umair.hotel_mangement_system_in_spring_boot.utility.Message;
+import org.umair.hotel_mangement_system_in_spring_boot.utility.Responses;
+
+@Service
+public class BookingService {
+
+    @Autowired
+    BookingRepostory bookingRepostory;
+
+    public Responses createBooking(Booking booking){
+
+        try {
+            Responses responses = new Responses();
+            Booking booking1 = bookingRepostory.save(booking);
+            if (booking1 == null){
+                throw new Exception("There is some error occurred while saving booking.");
+            }
+            responses.setResponse("Data",booking1);
+            responses.setResponse("mainMessage",new Message("Booking created",true));
+            return responses;
+        } catch (Exception e) {
+            Responses responses = new Responses();
+            responses.setResponse("mainMessage",new Message("There is error in Booking = " + e,false));
+            return responses;
+        }
+
+    }
+
+}
